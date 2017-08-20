235e182f8f40f9d8b1e4dc71293d226d3b129969
Michael <rhino1181@hotmail.co.uk>
2017-08-20 19:08:55 +0100

    Woops

--- MineC++/Shaders/VertexShader.glsl
+++ MineC++/Shaders/VertexShader.glsl
@@ -8,7 +8,7 @@ out vec3 fragmentColor;
 void main()
 {
 	//gl_Position = MVP * vec4(vertexPosition,1);
-	gl_Position = MVP * vec4(vertexPosition, 1.0);
+	gl_Position = vec4(vertexPosition, 1.0);
 
 	fragmentColor = vertexColor;
 }
