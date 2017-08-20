9a12485b09aa729215e458924323fa8e9777756f
Michael <rhino1181@hotmail.co.uk>
2017-08-20 23:03:39 +0100

    Changed chunk size

--- MineC++/Chunk.cpp
+++ MineC++/Chunk.cpp
@@ -14,32 +14,32 @@ Chunk::~Chunk()
 
 void Chunk::GenerateCubeData()
 {
-	mCubeData.resize(32);
-	for (size_t i = 0; i < 32; ++i)
+	mCubeData.resize(16);
+	for (size_t i = 0; i < 16; ++i)
 	{
-		mCubeData.at(i).resize(32);
+		mCubeData.at(i).resize(16);
 
-		for (size_t j = 0; j < 32; ++j)
+		for (size_t j = 0; j < 16; ++j)
 		{
 			mCubeData.at(i).at(j).resize(64);
 		}
 	}
-	for (size_t x = 0; x < 32; ++x)
+	for (size_t x = 0; x < 16; ++x)
 	{
-		for (size_t z = 0; z < 32; ++z)
+		for (size_t z = 0; z < 16; ++z)
 		{
 			for (size_t y = 0; y < 64; ++y)
 			{
-				if (x == 0 || y == 0 || z == 0 || x == 31 || y == 63 || z == 31)
+				if (x == 0 || y == 0 || z == 0 || x == 15 || y == 63 || z == 15)
 					mCubeData[x][z][y] = new Cube(CubeData::CubeType::AIR);
 				else
 					mCubeData[x][z][y] = new Cube(CubeData::CubeType::DIRT);
 			}
 		}
 	}
-	for (size_t x = 1; x < 32 - 1; ++x)
+	for (size_t x = 1; x < 16 - 1; ++x)
 	{
-		for (size_t z = 1; z < 32 - 1; ++z)
+		for (size_t z = 1; z < 16 - 1; ++z)
 		{
 			for (size_t y = 1; y < 64 - 1; ++y)
 			{
