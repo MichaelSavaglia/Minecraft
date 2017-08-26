d4ab2bce68b8a93babfc83a27b5f7cc23c87373e
Michael <rhino1181@hotmail.co.uk>
2017-08-26 12:41:47 +0100

    Instancing and sections added

---
+++ Core.cpp
@@ -0,0 +1,207 @@
+#include "Core.h"
+
+#include <glm.hpp>
+
+#include <vector>
+#include <iostream>
+#include <glew.h>
+#include <glfw3.h>
+
+#include "Shaders/ShaderLoader.h"
+#include "Camera.h"
+#include "CubeData.h"
+#include "Chunk.h"
+#include "Cube.h"
+#include "Section.h"
+
+Core::Core()
+{
+}
+
+
+Core::~Core()
+{
+}
+
+bool Core::Init()
+{
+	if (!glfwInit())
+	{
+		std::cout << "Failed to initialise GLFW" << std::endl;
+		return false;
+	}
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+
+
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(1280, 720, "MineC++", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to open GLFW Window" << std::endl;
+
+		glfwTerminate();
+
+		return false;
+	}
+
+	glfwMakeContextCurrent(window);
+	glewExperimental = true;
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialise GLEW" << std::endl;
+		return false;
+	}
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+	auto start = glfwGetTime();
+	Section* testSect = new Section(0);
+	auto posData = testSect->GenPosData();
+	for (size_t i = 0; i < 1000; ++i)
+	{
+		testSect->GenPosData();
+	}
+	auto end = glfwGetTime();
+	std::cout << (end - start) / 1000 << std::endl;
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+
+	GLuint posBuffer;
+	glGenBuffers(1, &posBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, posBuffer);
+	glBufferData(GL_ARRAY_BUFFER, posData.size() * sizeof(GLfloat), &posData[0], GL_STATIC_DRAW);
+
+	GLuint cubeBuffer;
+	glGenBuffers(1, &cubeBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeBuffer);
+	glBufferData(GL_ARRAY_BUFFER, CubeData::mVertices.size() * sizeof(GLfloat), &CubeData::mVertices[0], GL_STATIC_DRAW);
+
+	/*GLuint colorBuffer;
+	glGenBuffers(1, &colorBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+	glBufferData(GL_ARRAY_BUFFER, chunk->mChunkColours.size() * sizeof(GLfloat), &chunk->mChunkColours[0], GL_STATIC_DRAW);*/
+
+	//GLuint uvBuffer;
+	//glGenBuffers(1, &uvBuffer);
+	//glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+	//glBufferData(GL_ARRAY_BUFFER, chunk->mChunkUVs.size() * sizeof(GLfloat), &chunk->mChunkUVs[0], GL_STATIC_DRAW);
+
+	GLuint indexBuffer;
+	glGenBuffers(1, &indexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, CubeData::mIndices.size() * sizeof(unsigned short), &CubeData::mIndices[0], GL_STATIC_DRAW);
+
+
+	GLuint ProgramID;
+	ProgramID = ShaderUtil::LoadShaders("Shaders/VertexShader.glsl", "Shaders/FragmentShader.glsl");
+
+	glm::mat4 Model = glm::mat4(1.0f);
+
+	GLuint MatrixID = glGetUniformLocation(ProgramID, "MVP");
+
+	Camera* cam = new Camera(window);
+	
+	GLuint textureID;// = SOIL_load_OGL_texture("Textures/dirt.png", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT);
+
+	glGenTextures(1, &textureID);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		int width, height;
+		unsigned char* image = SOIL_load_image("Textures/dirt.png", &width, &height, 0, SOIL_LOAD_RGB);// SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+		SOIL_free_image_data(image);
+	GLuint TextureSampler = glGetUniformLocation(ProgramID, "myTextureSampler");
+	
+
+	double lastTime = glfwGetTime();
+	int nbFrames = 0;
+	do
+	{
+
+		// Measure speed
+		double currentTime = glfwGetTime();
+		nbFrames++;
+		if (currentTime - lastTime >= 1.0) { // If last prinf() was more than 1 sec ago
+												// printf and reset timer
+			printf("%f ms/frame\n", 1000.0 / double(nbFrames));
+			nbFrames = 0;
+			lastTime += 1.0;
+		}
+
+		cam->Update(true);
+		auto projection = cam->GetProjectionMatrix();
+		auto view = cam->GetViewMatrix();
+		glEnable(GL_CULL_FACE);
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LESS);
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glUseProgram(ProgramID);
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glUniform1i(TextureSampler, 0);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, cubeBuffer);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, posBuffer);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		
+		glm::mat4 mvp = projection * view * Model;
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+		glVertexAttribDivisor(1, 1);
+
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+		//glDrawElements(GL_TRIANGLES, CubeData::mIndices.size(), GL_UNSIGNED_SHORT, 0);
+		glDrawElementsInstanced(GL_TRIANGLES, CubeData::mIndices.size(), GL_UNSIGNED_SHORT, 0, 4096);
+		//glDisableVertexAttribArray(0);
+		//glDisableVertexAttribArray(1);
+		
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
+
+	return true;
+}
+
+void Core::LoadObjects()
+{
+}
+
+void Core::MainLoop()
+{
+}
+
+void Core::Draw()
+{
+}
+
+void Core::Update()
+{
+}
+
+void Core::Input()
+{
+}
---
+++ Section.cpp
@@ -0,0 +1,29 @@
+#include "Section.h"
+#include <glfw3.h>
+#include <iostream>
+
+Section::Section(Types::uint8 yPos) : mYPos(yPos)
+{
+	for (size_t i = 0; i < 4096; ++i)
+	{
+		mBlockIDs[i] = CubeData::CubeType::DIRT;
+	}
+}
+
+
+Section::~Section()
+{
+}
+
+std::vector<GLfloat> Section::GenPosData()
+{
+	std::vector<GLfloat> out;
+	out.reserve(4096 * 3);
+	for (size_t i = 0; i < 4096; ++i)
+	{
+		out.push_back(i & 0xF);
+		out.push_back((i >> 4) & 0xF);
+		out.push_back((i >> 8) & 0xF);
+	}
+	return out;
+}
