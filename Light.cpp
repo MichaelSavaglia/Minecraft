146e829d80f0d6ca449758db2d213c40ea218c4f
HarmedTwins <harmedtwins@gmail.com>
2017-08-31 21:51:52 +0100

    Oops

---
+++ Light.cpp
@@ -0,0 +1,13 @@
+#include "Light.h"
+
+
+
+Light::Light(float x, float y, float z)
+{
+	
+}
+
+
+Light::~Light()
+{
+}
---
+++ Light.h
@@ -0,0 +1,11 @@
+#pragma once
+class Light
+{
+public:
+	Light(float x, float y, float z);
+	~Light();
+
+private:
+	float mX, mY, mZ;
+};
+
